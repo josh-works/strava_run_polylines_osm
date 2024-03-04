b68dadb7d7fde6d26405774a5884389250f8af95
Josh Thompson <thompsonjoshd@gmail.com>
2024-03-04 10:28:10 -0700

    outline /felt

--- app.py
+++ app.py
@@ -46,5 +46,35 @@ def my_runs():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/felt')
+def felt():
+    latlng_param = ""
+    initial_zoom_level = "15"
+    if request.args.get('latlng'):
+        latlng_param = str(request.args.get('latlng'))
+    
+    if request.args.get('zoom'):
+        initial_zoom_level = str(request.args.get('zoom'))
+    runs = []
+    with open("runs.csv", "r") as runs_file:
+        reader = csv.DictReader(runs_file)
+        for row in reader:
+            runs.append(row["polyline"])
+
+    photos = []
+    with open("pictures.csv", "r") as photos_file:
+        reader = csv.DictReader(photos_file)
+        for row in reader:
+            photos.append([row["url"], row["latlng"]])
+
+    print(photos[0])
+    return render_template("felt.html", 
+                           runs = json.dumps(runs), 
+                           latlng = latlng_param, 
+                           zoom = initial_zoom_level,
+                            photos = json.dumps(photos) )
+
+
 if __name__ == "__main__":
     app.run(port = 5001)
